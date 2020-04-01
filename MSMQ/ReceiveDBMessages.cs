21ad345db85d4fe8fbaba32320479ae758df1f18
Jefferson <djefferson@acgov.org>
2020-03-31 19:29:08 -0700

    Add project files.

---
+++ MSMQ/ReceiveDBMessages.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RabbitMQ.Util;
+
+namespace MSMQ2
+{
+    class ReceiveDBMessages
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                Console.WriteLine("Setting up queue");
+                channel.QueueDeclare(queue: "SSMessage", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [x] Received {0}", message);
+                };
+                channel.BasicConsume(queue: "SSMessage", noAck: true, consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
+
---
+++ MSMQ/SendDBMessages.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace MSMQ2
+{
+    class SendDBMessages
+    {
+        public static void Main()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "SSMessage", durable: false, exclusive: false, autoDelete: false, arguments: null);
+                for (int i = 0; i < 10; i++)
+                {
+                    string message = "Select Server Data Here";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "", routingKey: "SSMessage", basicProperties: null, body: body);
+                    Console.WriteLine(i + " [x] Sent {0}", message);
+                }
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+
+}
